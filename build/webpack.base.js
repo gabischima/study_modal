c380f889bf7a36f1f3a76f34f93b6532cf5e3094
Gabriela <gabismauricio@gmail.com>
2019-09-01 00:51:50 -0300

    :arrow_up: upgrade dependencies

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
@@ -26,10 +26,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
+          loader: 'babel-loader'
         }
       },
       {
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,22 +1,21 @@
 const base = require('./webpack.base.js')
 const merge = require('webpack-merge')
 const webpack = require('webpack')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = () => merge(base, {
 	mode: 'production',
 	output: {
 		filename: 'gsm-modal.min.js'
 	},
+	optimization: {
+    minimizer: [new TerserPlugin()]
+  },
 	plugins: [
 		new webpack.EnvironmentPlugin({
 			NODE_ENV: 'production',
 			API_URL: 'url',
 			DEBUG: false
-		}),
-		new UglifyJsPlugin({
-			test: /\.js($|\?)/i,
-			sourceMap: false
 		})
 	]
 })
